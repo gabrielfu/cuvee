575cb1222f14638c1ef54c5e70790b2956eafa72
Gabriel Fu <hfu.gabriel@gmail.com>
2024-03-15 11:04:37 +0800

    move font to css

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -3,18 +3,3 @@
 </script>
 
 <slot />
-
-<style global>
-  @font-face {
-      font-family: 'Schuss';
-      font-style: normal;
-      font-weight: 400;
-      src: url('../static/fonts/SchussSerifPro-Regular.woff2') format("woff2");
-  }
-
-  :global(html) {
-    font-family: 'Schuss';
-    font-size: 20px;
-    font-style: italic;
-  }
-</style>
