a27bc2fb45cccc661f97cac8294a6ee88768f8cd
Gabriel Fu <hfu.gabriel@gmail.com>
2024-03-15 15:13:55 +0800

    add footer

---
+++ frontend/src/components/Footer.svelte
@@ -0,0 +1,12 @@
+<script>
+  import Icon from "./Icon.svelte";
+</script>
+
+<footer>
+  <div class="flex flex-col gap-2 justify-center items-center py-4">
+    <a href="https://github.com/gabrielfu/cuvee" target="_blank">
+      <Icon name="github" />
+    </a>
+    <p class="fira-sans text-[16px]">©2024 cuvée. All rights reserved.</p>
+</div>
+</footer>
