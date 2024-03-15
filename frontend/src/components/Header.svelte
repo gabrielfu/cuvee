87e9e7af50147f6ac16e97aad56aaacc36c3b636
Gabriel Fu <hfu.gabriel@gmail.com>
2024-03-15 14:45:57 +0800

    Add header

---
+++ frontend/src/components/Header.svelte
@@ -0,0 +1,16 @@
+<header>
+  <div class="
+    sticky top-0 pt-4 pb-8 flex justify-center items-center
+    border-b border-solid border-primary
+  ">
+    <div class="absolute left-1/2 transform -translate-x-1/2">
+      <h1 class="monarda text-[3rem]">cuvée</h1>
+    </div>
+    <div class="ml-auto pr-10 translate-y-1/3">
+      Register
+    </div>
+    <div class="pr-10 translate-y-1/3">
+      Sign in
+    </div>
+  </div>
+</header>
