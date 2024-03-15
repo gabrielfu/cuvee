a5ff1c10e5df7448ff517a3c3e8fe8c7ad52bbfe
Gabriel Fu <hfu.gabriel@gmail.com>
2024-03-15 11:03:20 +0800

    add schuss font

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,5 +1,20 @@
-<script>
+<script lang="ts">
   import "../app.css";
 </script>
 
 <slot />
+
+<style global>
+  @font-face {
+      font-family: 'Schuss';
+      font-style: normal;
+      font-weight: 400;
+      src: url('../static/fonts/SchussSerifPro-Regular.woff2') format("woff2");
+  }
+
+  :global(html) {
+    font-family: 'Schuss';
+    font-size: 20px;
+    font-style: italic;
+  }
+</style>
