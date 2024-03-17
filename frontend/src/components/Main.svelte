d1f1ff432c1aeaba50553f85c08385755e996be1
Gabriel Fu <hfu.gabriel@gmail.com>
2024-03-17 12:08:04 +0800

    update main

--- frontend/src/components/Main.svelte
+++ frontend/src/components/Main.svelte
@@ -81,42 +81,44 @@
   {#each wines as wine, i}
   <Card.Card class="bg-card text-card-foreground">
     <Card.Content>
-      <div class="grid grid-cols-[1fr,3fr] pt-4 pb-2">
-        <img src={images[i]} alt={wine.name} width="256" />
+      <div class="grid grid-cols-[1fr,3fr] pt-4">
+        <img src={images[i]} alt={wine.name} width="224" />
         <div>
           <Card.Title class="font-normal fira-sans my-4">
             {wine.name.toUpperCase()}
           </Card.Title>
           <p class="mb-4">{wine.vintage}&#xff5c;{wine.region}&#xff5c;{wine.country}</p>
-          <p class="fira-sans text-sm">PURCHASES</p>
+          <p class="fira-sans text-sm mb-2">PURCHASES</p>
           {#if !detailedView}
-          <Card.Card class="w-48 px-4 py-2">
-            <p>
+          <Card.Card class="w-1/4 mx-2 px-4 py-2">
+            <p class="fira-sans text-sm">
               <i class="fa-solid fa-bottle-droplet text-sm" /> &nbsp;{aggregatedPurchases[i].quantity} bottle{aggregatedPurchases[i].quantity > 1 ? 's' : ''}
             </p>
-            <p>
+            <p class="fira-sans text-sm">
               <i class="fa-solid fa-dollar-sign text-sm" /> &nbsp;HKD {aggregatedPurchases[i].price.toFixed(2)}
             </p>
           </Card.Card>
           {:else}
-          <Carousel.Root>
-            <Carousel.Content class="-ml-2">
+          <Carousel.Root class="mx-12">
+            <Carousel.Content>
               {#each wine.purchases as p}
-              <Carousel.Item class="basis-1/3 pl-4">
+              <Carousel.Item class="basis-1/4 pl-4">
                 <Card.Card class="px-4 py-2">
-                  <p>
+                  <p class="fira-sans text-sm">
                     <i class="fa-solid fa-bottle-droplet text-sm" /> &nbsp;{p.quantity} bottle{p.quantity > 1 ? 's' : ''}
                   </p>
-                  <p>
+                  <p class="fira-sans text-sm">
                     <i class="fa-solid fa-dollar-sign text-sm" /> &nbsp;HKD {p.price}
                   </p>
-                  <p>
+                  <p class="fira-sans text-sm">
                     <i class="fa-regular fa-calendar text-sm" /> &nbsp;{p.date.toISOString().slice(0, 10)}
                   </p>
                 </Card.Card>
               </Carousel.Item>
               {/each}
             </Carousel.Content>
+            <Carousel.Previous />
+            <Carousel.Next />
           </Carousel.Root>
           {/if}
         </div>
