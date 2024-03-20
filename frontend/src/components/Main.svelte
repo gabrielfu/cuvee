dcd96e7fdefe38ea78b698515ecc3f8c4c71318f
Gabriel Fu <hfu.gabriel@gmail.com>
2024-03-20 18:04:13 +0800

    simplified detailed purchases as collapsible table

--- frontend/src/components/Main.svelte
+++ frontend/src/components/Main.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
   import * as Card from "$lib/components/ui/card";
-  import * as Carousel from "$lib/components/ui/carousel";
+  import * as Collapsible from "$lib/components/ui/collapsible";
   import { Input } from "$lib/components/ui/input";
   import { Label } from "$lib/components/ui/label";
   import { Switch } from "$lib/components/ui/switch";
   import * as Select from "$lib/components/ui/select";
+  import * as Table from "$lib/components/ui/table";
 
   type Vintage = number | "NV";
   type Purchase = {
@@ -71,8 +72,6 @@
     return { quantity: totalQuantity, price: totalPrice / totalQuantity };
   };
 
-  let detailedView = false;
-
   const sortBys = {
     "default": "Default",
     "name_asc": "Name (asc)",
@@ -95,11 +94,6 @@
     <Input class="max-w-sm h-[36px]" type="text" id="search" placeholder="Search" />
 
     <div class="flex items-center space-x-2 my-4 px-2">
-      <Label for="detailed-view" class="font-normal">Purchase Details</Label>
-      <Switch id="detailed-view" bind:checked={detailedView} />
-    </div>
-
-    <div class="flex items-center space-x-2 my-4 px-2">
       <Label class="font-normal pr-1">Sort By</Label>
       <Select.Root selected={sortBy}>
         <Select.Trigger class="w-[180px] h-[36px]">
@@ -116,6 +110,7 @@
   </div>
 
   {#each wines as wine, i}
+  {@const p = aggregatePurchase(wine.purchases)}
   <Card.Card class="bg-card text-card-foreground">
     <Card.Content>
       <div class="grid grid-cols-[minmax(224px,1fr),3fr,2fr] pt-4">
@@ -124,10 +119,10 @@
           <Card.Title class="font-medium mt-4 mb-2">
             {wine.name}
           </Card.Title>
+
           <p class="text-sm text-muted-foreground mb-4">{wine.vintage}&#xff5c;{wine.format}&#xff5c;{wine.region}&#xff5c;{wine.country}</p>
-          {#if !detailedView}
-          {@const p = aggregatePurchase(wine.purchases)}
-          <Card.Card class="w-1/3 mx-2 px-4 py-2">
+
+          <Card.Card class="w-1/3 mx-2 px-4 py-2 mb-2">
             <p class="text-sm">
               <i class="fa-solid fa-bottle-droplet text-sm" /> &nbsp;{p.quantity} bottle{p.quantity > 1 ? 's' : ''}
             </p>
@@ -135,29 +130,30 @@
               <i class="fa-solid fa-dollar-sign text-sm" /> &nbsp;{p.price.toFixed(2)}
             </p>
           </Card.Card>
-          {:else}
-          <Carousel.Root class="mx-12">
-            <Carousel.Content>
-              {#each wine.purchases.sort().reverse() as p}
-              <Carousel.Item class="basis-1/3 pl-4">
-                <Card.Card class="px-4 py-2">
-                  <p class="text-sm">
-                    <i class="fa-solid fa-bottle-droplet text-sm" /> &nbsp;{p.quantity} bottle{p.quantity > 1 ? 's' : ''}
-                  </p>
-                  <p class="text-sm">
-                    <i class="fa-solid fa-dollar-sign text-sm" /> &nbsp;{p.price}
-                  </p>
-                  <p class="text-sm">
-                    <i class="fa-regular fa-calendar text-sm" /> &nbsp;{p.date.toISOString().slice(0, 10)}
-                  </p>
-                </Card.Card>
-              </Carousel.Item>
-              {/each}
-            </Carousel.Content>
-            <Carousel.Previous />
-            <Carousel.Next />
-          </Carousel.Root>
-          {/if}
+
+          <Collapsible.Root class="pl-4">
+            <Collapsible.Trigger class="text-sm text-muted-foreground">Expand purchases <i class="fa-solid fa-chevron-down text-sm" /></Collapsible.Trigger>
+            <Collapsible.Content>
+              <Table.Root class="w-auto">
+                <Table.Header>
+                  <Table.Row class="leading-none">
+                    <Table.Head class="h-auto py-1 text-sm"><i class="fa-solid fa-bottle-droplet text-sm" /></Table.Head>
+                    <Table.Head class="h-auto py-1 text-sm"><i class="fa-solid fa-dollar-sign text-sm" /></Table.Head>
+                    <Table.Head class="h-auto py-1 text-sm"><i class="fa-regular fa-calendar text-sm" /></Table.Head>
+                  </Table.Row>
+                </Table.Header>
+                <Table.Body>
+                  {#each [...wine.purchases].sort().reverse() as entry}
+                  <Table.Row class="leading-none">
+                    <Table.Cell class="py-1 text-sm">{entry.quantity}</Table.Cell>
+                    <Table.Cell class="py-1 text-sm">{entry.price}</Table.Cell>
+                    <Table.Cell class="py-1 text-sm">{entry.date.toISOString().slice(0, 10)}</Table.Cell>
+                  </Table.Row>
+                  {/each}
+                </Table.Body>
+              </Table.Root>
+            </Collapsible.Content>
+          </Collapsible.Root>
         </div>
         <div>
 
