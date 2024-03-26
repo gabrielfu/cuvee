71a440495ddcc638a4f912b0fac11ca4cd25e69a
Gabriel Fu <hfu.gabriel@gmail.com>
2024-03-26 16:07:26 +0800

    refactor WineCatalogNavbar

--- frontend/src/components/cellar/WineCatalog.svelte
+++ frontend/src/components/cellar/WineCatalog.svelte
@@ -1,50 +1,16 @@
 <script lang="ts">
-  import { Input } from "$lib/components/ui/input";
-  import { Label } from "$lib/components/ui/label";
-  import * as Select from "$lib/components/ui/select";
-
   import type { Wine } from "$lib/api/wines";
 	import WineCard from "./WineCard.svelte";
+	import WineCatalogNavbar from "./WineCatalogNavbar.svelte";
 
   export let wines: Wine[];
   export let images: string[];
-
-  const sortBys = {
-    "default": "Default",
-    "name_asc": "Name (asc)",
-    "name_desc": "Name (desc)",
-    "vintage_asc": "Vintage (asc)",
-    "vintage_desc": "Vintage (desc)",
-    "price_asc": "Price (asc)",
-    "price_desc": "Price (desc)",
-    "newest": "Newest",
-    "oldest": "Oldest",
-  };
-  let sortBy = { value: "default", label: sortBys["default"] };
 </script>
 
 <main>
-  <p class="fira-sans font-bold text-3xl px-2">MY CELLAR </p>
+  <h1 class="fira-sans font-bold text-3xl px-2">MY CELLAR </h1>
 
-  <div class="flex items-center justify-between">
-
-    <Input class="max-w-sm h-[36px]" type="text" id="search" placeholder="Search" />
-
-    <div class="flex items-center space-x-2 my-4 px-2">
-      <Label class="font-normal pr-1">Sort By</Label>
-      <Select.Root selected={sortBy}>
-        <Select.Trigger class="w-[180px] h-[36px]">
-          <Select.Value class="text-sm" placeholder="" />
-        </Select.Trigger>
-        <Select.Content>
-          {#each Object.entries(sortBys) as [value, label]}
-          <Select.Item class="text-sm" value={value}>{label}</Select.Item>
-          {/each}
-        </Select.Content>
-      </Select.Root>
-    </div>
-
-  </div>
+  <WineCatalogNavbar />
 
   {#each wines as wine, i}
   <WineCard wine={wine} image={images[i]} />
---
+++ frontend/src/components/cellar/WineCatalogNavbar.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+  import { Input } from "$lib/components/ui/input";
+  import { Label } from "$lib/components/ui/label";
+  import * as Select from "$lib/components/ui/select";
+
+  const sortBys = {
+    "default": "Default",
+    "name_asc": "Name (asc)",
+    "name_desc": "Name (desc)",
+    "vintage_asc": "Vintage (asc)",
+    "vintage_desc": "Vintage (desc)",
+    "price_asc": "Price (asc)",
+    "price_desc": "Price (desc)",
+    "newest": "Newest",
+    "oldest": "Oldest",
+  };
+  let sortBy = { value: "default", label: sortBys["default"] };
+</script>
+
+<div class="flex items-center justify-between">
+  <Input class="max-w-sm h-[36px]" type="text" id="search" placeholder="Search" />
+
+  <div class="flex items-center space-x-2 my-4 px-2">
+    <Label class="font-normal pr-1">Sort By</Label>
+    <Select.Root selected={sortBy}>
+      <Select.Trigger class="w-[180px] h-[36px]">
+        <Select.Value class="text-sm" placeholder="" />
+      </Select.Trigger>
+      <Select.Content>
+        {#each Object.entries(sortBys) as [value, label]}
+        <Select.Item class="text-sm" value={value}>{label}</Select.Item>
+        {/each}
+      </Select.Content>
+    </Select.Root>
+  </div>
+</div>
