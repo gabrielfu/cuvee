fadd380cb328463442748cbb42f8cb67d35c8c5d
Gabriel Fu <hfu.gabriel@gmail.com>
2024-03-26 17:41:04 +0800

    bind selected

--- frontend/src/components/cellar/WineCatalogNavbar.svelte
+++ frontend/src/components/cellar/WineCatalogNavbar.svelte
@@ -2,6 +2,7 @@
   import { Input } from "$lib/components/ui/input";
   import { Label } from "$lib/components/ui/label";
   import * as Select from "$lib/components/ui/select";
+	import type { Selected } from "bits-ui";
 
   const sortBys = {
     "default": "Default",
@@ -14,16 +15,18 @@
     "newest": "Newest",
     "oldest": "Oldest",
   };
-  let sortBy = { value: "default", label: sortBys["default"] };
+  let selected = { value: "default", label: sortBys["default"] };
+
+  export let onSelectedChange: (e: any) => void;
 </script>
 
 <div class="flex items-center justify-between">
-  <Input class="max-w-sm h-[36px]" type="text" id="search" placeholder="Search" />
+  <Input class="max-w-sm h-[40px]" type="text" id="search" placeholder="Search" />
 
   <div class="flex items-center space-x-2 my-4 px-2">
     <Label class="font-normal pr-1">Sort By</Label>
-    <Select.Root selected={sortBy}>
-      <Select.Trigger class="w-[180px] h-[36px]">
+    <Select.Root bind:selected={selected} onSelectedChange={onSelectedChange}>
+      <Select.Trigger class="w-[180px] h-[40px]">
         <Select.Value class="text-sm" placeholder="" />
       </Select.Trigger>
       <Select.Content>
