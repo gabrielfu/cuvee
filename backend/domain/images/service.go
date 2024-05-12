2167460cc9d5d85e0efad8dd6cc33118ea8d5185
Gabriel Fu <hfu.gabriel@gmail.com>
2024-05-12 15:29:48 +0800

    validate image

--- backend/domain/images/service.go
+++ backend/domain/images/service.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"net/http"
 	"net/url"
+	"strings"
 )
 
 type ImageService struct {
@@ -46,6 +47,21 @@ func NewImageService(googleSearchCx string, googleSearchApiKey string) *ImageSer
 	return &ImageService{googleSearchCx: googleSearchCx, googleSearchApiKey: googleSearchApiKey}
 }
 
+var imageExtensions = []string{".jpg", ".jpeg", ".png", ".webp"}
+
+func validateImageExtensions(link string) bool {
+	for _, ext := range imageExtensions {
+		if strings.HasSuffix(link, ext) {
+			return true
+		}
+	}
+	return false
+}
+
+func validateImageLink(link string) bool {
+	return strings.HasPrefix(link, "https://") && validateImageExtensions(link)
+}
+
 func (s *ImageService) Search(ctx context.Context, request ImageSearchRequest) (ImageSearchResponse, error) {
 	query := url.QueryEscape("vivino " + request.name + " " + request.vintage + " " + request.country + " " + request.region)
 	reqUrl := "https://content-customsearch.googleapis.com/customsearch/v1?searchType=image&q=" + query + "&cx=" + s.googleSearchCx + "&key=" + s.googleSearchApiKey
@@ -54,7 +70,7 @@ func (s *ImageService) Search(ctx context.Context, request ImageSearchRequest) (
 	if resp.StatusCode != http.StatusOK {
 		p := make([]byte, 1024)
 		resp.Body.Read(p)
-		msg := fmt.Errorf("received %d response code from Google Image Search: %s\n", resp.StatusCode, string(p))
+		msg := fmt.Errorf("received %d response code from Google Image Search: %s", resp.StatusCode, string(p))
 		log.Printf("Google Image Search error (status=%d): %v\n", resp.StatusCode, msg)
 		return ImageSearchResponse{}, err
 	}
@@ -72,6 +88,9 @@ func (s *ImageService) Search(ctx context.Context, request ImageSearchRequest) (
 
 	response := ImageSearchResponse{}
 	for _, item := range searchResponse.Items {
+		if !validateImageLink(item.Link) {
+			continue
+		}
 		response.Items = append(response.Items, ImageSearchResponseItem{
 			Link:   item.Link,
 			Height: item.Image.Height,
