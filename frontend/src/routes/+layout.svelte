70936eaee6446aa6f28c46b47d422fe518f97089
Gabriel Fu <hfu.gabriel@gmail.com>
2024-05-14 11:22:45 +0800

    fix go top button blocking page bottom

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -17,7 +17,7 @@
   class="container relative flex flex-col max-w-[1400px] mx-auto w-full text-sm sm:text-base min-h-screen"
 >
   <div
-    class={"fixed bottom-0 w-full duration-200 flex p-10 z-[10] " +
+    class={"fixed bottom-10 right-10 duration-200 flex z-10 " +
       (y > 0 ? " opacity-full pointer-events-auto" : " pointer-events-none opacity-0")}
   >
     <button
